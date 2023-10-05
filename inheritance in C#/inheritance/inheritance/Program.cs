a15bcb85b95af62d6e9ce7a585ee22a5e2f30931
Fareed Khan <127206523+Fareedkhan56@users.noreply.github.com>
2023-10-05 19:26:34 +0500

    Add files via upload

---
+++ inheritance in C#/inheritance/inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class car:Program
+    {
+       public void carSpecifications()
+        {
+            Console.WriteLine("color : blue");
+            Console.WriteLine("Model : SupraMK4");
+            Console.WriteLine("Price : 50000000");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car sp = new car();
+            sp.carSpecifications();
+            Console.ReadLine();
+        }
+    }
+}
